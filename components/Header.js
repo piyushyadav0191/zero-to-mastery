579065f8b2725dfc8b8d66564be19753665c2ce8
piyushyadav0191 <piyushyadav0191@gmail.com>
2022-12-09 09:43:48 +0530

    arrays more questions

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className='px-12'>
+        <input checked id="checked-checkbox" type="checkbox" value="" class="w-12 h-12 text-green-600 bg-gray-100 rounded border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+      </div>
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
+        <Link href="https://zerotomastery.io/">
           <a>{name}</a>
         </Link>
       </p>
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Piyush Yadav';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Zero to Mastery Certification';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
